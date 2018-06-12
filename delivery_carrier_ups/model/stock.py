b054b3e4f2b1724954b29a2b6f3ed57f3bed8ad0
Anna Martínez <ana.martinez@diagram.es>
2018-06-12 10:04:52 +0200

    Revert "Mail Notification"

--- delivery_carrier_ups/model/stock.py
+++ delivery_carrier_ups/model/stock.py
@@ -88,7 +88,6 @@ class StockPicking(models.Model):
             'country': warehouse_partner.country_id.code,
             'postal_code': warehouse_partner.zip,
             'phone': warehouse_partner.phone or '',
-            'email': warehouse_partner.email or ''
         }
 
         to_addr = {
@@ -99,7 +98,6 @@ class StockPicking(models.Model):
             'country': self.partner_id.country_id.code,
             'postal_code': self.partner_id.zip,
             'phone': self.partner_id.mobile or self.partner_id.phone or '',
-            'email': self.partner.email or ''
         }
 
         number_of_packages = self.number_of_packages or 1
